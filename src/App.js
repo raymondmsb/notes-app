c1602d24047d577fad675360046c9f54c117597b
Ray Bach <raymondmsbach@gmail.com>
2024-04-02 12:39:44 -0500

    Display list of notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useReducer} from 'react';
+import { generateClient } from 'aws-amplify/api';
+import {List} from 'antd';
+import {listNotes} from './graphql/queries';
+
+const initialState = {
+  notes: [], 
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NOTES':
+      return {
+       ...state,
+        notes: action.notes,
+        loading: false
+      };
+    case 'ERROR':
+      return {
+       ...state,
+        loading: false,
+        error: true
+      };
+    default:
+      return { ...state};
+  }
+};
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNotes = async() => {
+    try {
+      const client = generateClient();
+      const notesData = await client.graphql({ query: listNotes });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+    } catch (error) {
+      dispatch({ type: 'ERROR' });
+    }
+  }
+
+  const renderItem = (item) => {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
+    );
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: { textAlign: 'left' },
+    p: {color: '#1890ff'}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <List 
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 }
