ece8c81f456ec7f2b8237877a7fcb4a4546c59c5
Ray Bach <raymondmsbach@gmail.com>
2024-04-02 13:10:00 -0500

    Add note functionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import React, {useEffect, useReducer} from 'react';
 import { generateClient } from 'aws-amplify/api';
 import {listNotes} from './graphql/queries';
-import { v4 as uuid } from 'antd';
+import { v4 as uuid } from 'uuid';
 import { List, Input, Button } from 'antd';
 import {createNote as CreateNote} from './graphql/mutations';
+
+const CLIENT_ID = uuid();
 
 const initialState = {
   notes: [], 
@@ -26,6 +28,25 @@ const reducer = (state, action) => {
        ...state,
         loading: false,
         error: true
+      };
+    case 'ADD_NOTE':
+      return {
+       ...state,
+        notes: [...state.notes, action.note],
+        loading: false
+      };
+    case 'RESET_FORM':
+      return {
+       ...state,
+        form: initialState.form
+      };
+    case 'SET_INPUT':
+      return {
+       ...state,
+        form: {
+         ...state.form,
+          [action.name]: action.value
+        }
       };
     default:
       return { ...state};
@@ -68,8 +89,52 @@ function App() {
     p: {color: '#1890ff'}
   }
 
+  const createNote = async() => {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      const client = generateClient();
+      await client.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note')
+    } catch (err) {
+      console.error("error: ", err)
+    }
+  }
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  }
+
   return (
     <div style={styles.container}>
+      <Input 
+        onChange={onChange}
+        value={state.form.name}
+        placeholder='Note Name'
+        name='name'
+        style={styles.input}
+      />
+      <Input 
+        onChange={onChange}
+        value={state.form.description}
+        placeholder='Note Description'
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >
+        Create Note
+      </Button>
       <List 
         loading={state.loading}
         dataSource={state.notes}
